3e1b600b2e49bdf3c5d23d5fd95296bdb67a96dc
Francisco Valdez <87194400+Franciscoxvl@users.noreply.github.com>
2024-02-07 19:06:25 -0500

    Add files via upload

---
+++ includes/config/database.php
@@ -0,0 +1,13 @@
+<?php 
+
+function conectarDB() : mysqli {
+    $db = mysqli_connect('localhost', 'root', '', 'bienesraices_crud');
+
+    if(!$db) {
+        echo "Error no se pudo conectar";
+        exit;
+    } 
+
+    return $db;
+    
+}
